edde5fdb6309246b568a9becf4ccdaba4d5fbe2e
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-09-22 10:43:31 +0900

    Update: examples/l2fwd

--- examples/l2fwd/main.cc
+++ examples/l2fwd/main.cc
@@ -4,16 +4,16 @@
 
 int l2fwd(void*)
 {
+  const size_t n_ports = rte_eth_dev_count();
   while (true) {
-    constexpr size_t BURSTSZ = 32;
-    rte_mbuf* mbufs[BURSTSZ];
+    for (size_t pid=0; pid<n_ports; pid++) {
+      constexpr size_t BURSTSZ = 32;
+      rte_mbuf* mbufs[BURSTSZ];
 
-    constexpr size_t nb_q = 1;
-    size_t nb_recv = rte_eth_rx_burst(0, 0, mbufs, BURSTSZ);
-    if (nb_recv == 0) continue;
-    size_t nb_send = rte_eth_tx_burst(1, 0, mbufs, nb_recv);
-    if (nb_send < nb_recv)
-      dpdk::rte_pktmbuf_free_bulk(&mbufs[nb_send], nb_recv-nb_send);
+      size_t nb_recv = rte_eth_rx_burst(pid, 0, mbufs, BURSTSZ);
+      if (nb_recv == 0) continue;
+      rte_eth_tx_burst(pid^1, 0, mbufs, nb_recv);
+    }
   }
 }
 
@@ -24,11 +24,11 @@ int main(int argc, char** argv)
   dpdk::init_portconf(&port_conf);
   struct rte_mempool* mp = dpdk::mp_alloc("RXMBUFMP");
 
-  size_t nb_ports = rte_eth_dev_count();
-  if (nb_ports == 0) throw dpdk::exception("no ethdev");
-  printf("%zd ports found \n", nb_ports);
-  for (size_t i=0; i<nb_ports; i++) {
-    dpdk::port_configure(i, 2, 1, &port_conf, mp);
+  size_t n_ports = rte_eth_dev_count();
+  if (n_ports == 0) throw dpdk::exception("no ethdev");
+  printf("%zd ports found \n", n_ports);
+  for (size_t i=0; i<n_ports; i++) {
+    dpdk::port_configure(i, 1, 1, &port_conf, mp);
   }
 
   rte_eal_remote_launch(l2fwd, nullptr, 1);
