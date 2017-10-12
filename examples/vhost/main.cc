0c911269f71be8a2d7a17140362ef238ae4f1112
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-10-13 04:09:25 +0900

    WIP examples/vhost: refact

--- examples/vhost/main.cc
+++ examples/vhost/main.cc
@@ -1,15 +1,18 @@
 
 #include <stdio.h>
 #include <dpdk/dpdk.h>
-
+#include <slankdev/string.h>
+#include <slankdev/exception.h>
+using slankdev::exception;
+using slankdev::format;
 constexpr size_t nb_rxqueues = 1;
 constexpr size_t nb_txqueues = 1;
 
 int packet_capture(void*)
 {
-  const size_t nb_ports = rte_eth_dev_count();
+  const size_t n_port = rte_eth_dev_count();
   while (true) {
-    for (size_t pid=0; pid<nb_ports; pid++) {
+    for (size_t pid=0; pid<n_port; pid++) {
       for (size_t qid=0; qid<nb_rxqueues; qid++) {
         constexpr size_t BURSTSZ = 32;
         rte_mbuf* mbufs[BURSTSZ];
@@ -33,18 +36,18 @@ int packet_capture(void*)
 int main(int argc, char** argv)
 {
   dpdk::dpdk_boot(argc, argv);
+  size_t n_port = rte_eth_dev_count();
+  if (n_port != 1)
+    throw exception(format("n_port isn't 1(%zd)", n_port));
+
   struct rte_mempool* mp = dpdk::mp_alloc("RXMBUFMP");
-
-  size_t nb_ports = rte_eth_dev_count();
-  if (nb_ports == 0) throw dpdk::exception("no ethdev");
-
   struct rte_eth_conf port_conf;
   dpdk::init_portconf(&port_conf);
   port_conf.rxmode.mq_mode = ETH_MQ_RX_RSS;
   port_conf.rx_adv_conf.rss_conf.rss_key = NULL;
   port_conf.rx_adv_conf.rss_conf.rss_hf = ETH_RSS_IP|ETH_RSS_TCP|ETH_RSS_UDP;
-  printf("%zd ports found \n", nb_ports);
-  for (size_t i=0; i<nb_ports; i++) {
+  printf("%zd ports found \n", n_port);
+  for (size_t i=0; i<n_port; i++) {
     dpdk::port_configure(i, nb_rxqueues, nb_txqueues, &port_conf, mp);
   }
 
--- examples/vhost/startvm.sh
+++ examples/vhost/startvm.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
 
-QEMU=qemu-system-x86_64 
-MEM=1G
-SOCKPATH=/tmp/sock2
+QEMU=qemu-system-x86_64
+HDAPATH=/home/slank/qemu/u1604.qcow2
+SOCKPATH=/tmp/sock0
 HPPATH=/mnt/huge_kvm
 
 $QEMU \
 	-enable-kvm -cpu host \
-	-m $MEM -object memory-backend-file,id=mem,size=$MEM,mem-path=$HPPATH,share=on \
-	-mem-prealloc \
-	-numa node \
-	-hda u1604.qcow2 \
+	-m 512 -object memory-backend-file,id=mem,size=512M,mem-path=$HPPATH,share=on \
+	-numa node,memdev=mem -m 512 -mem-prealloc -debugcon file:debug.log \
+	-hda $HDAPATH \
 	-boot c -vnc :0,password -monitor stdio \
 	\
 	-chardev socket,id=chr0,path=$SOCKPATH \
 	-netdev vhost-user,id=net0,chardev=chr0,vhostforce,queues=1 \
 	-device virtio-net-pci,netdev=net0
-
 
