368bb59eac93b042e137124db396bd99c7aa0141
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-12-19 17:52:00 +0900

    2017.12.19-17:52

--- examples/l2fwd-sriov/main.cc
+++ examples/l2fwd-sriov/main.cc
@@ -2,8 +2,17 @@
 #include <stdio.h>
 #include <dpdk/dpdk.h>
 
-struct ether_addr dst[2];
-struct ether_addr src[2];
+struct ether_addr next_dst[2];
+struct ether_addr next_src[2];
+
+std::string to_str(const ether_addr* addr)
+{
+  std::string s;
+  for (size_t i=0; i<6; i++) {
+    s += dpdk::format("%02x%s", addr->addr_bytes[i], i<5?":":"");
+  }
+  return s;
+}
 
 constexpr size_t n_queues = 4;
 int l2fwd(void*)
@@ -18,24 +27,19 @@ int l2fwd(void*)
 				size_t nb_recv = rte_eth_rx_burst(pid, qid, mbufs, BURSTSZ);
 				if (nb_recv == 0) continue;
 
+        printf("recvfrom p%zd: ", pid);
         for (size_t i=0; i<nb_recv; i++) {
           uint8_t* p = rte_pktmbuf_mtod(mbufs[i], uint8_t*);
-          memcpy(p+0, &src[pid], 6);
-          memcpy(p+6, &dst[pid], 6);
+          memcpy(p+0, &next_dst[pid], 6);
+          memcpy(p+6, &next_src[pid], 6);
         }
+        printf(" src=%s dst=%s\n",
+            to_str(&next_src[pid]).c_str(),
+            to_str(&next_dst[pid]).c_str());
         rte_eth_tx_burst(pid^1, qid, mbufs, nb_recv);
 			}
     }
   }
-}
-
-std::string to_str(const ether_addr* addr)
-{
-  std::string s;
-  for (size_t i=0; i<6; i++) {
-    s += dpdk::format("%02x%s", addr->addr_bytes[i], i<5?":":"");
-  }
-  return s;
 }
 
 int main(int argc, char** argv)
@@ -53,25 +57,25 @@ int main(int argc, char** argv)
     dpdk::port_configure(i, n_queues, n_queues, &port_conf, mp);
   }
 
-  dst[0].addr_bytes[0] = 0x33;
-  dst[0].addr_bytes[1] = 0x33;
-  dst[0].addr_bytes[2] = 0x33;
-  dst[0].addr_bytes[3] = 0x33;
-  dst[0].addr_bytes[4] = 0x33;
-  dst[0].addr_bytes[5] = 0x33;
+  next_dst[0].addr_bytes[0] = 0x52;
+  next_dst[0].addr_bytes[1] = 0x54;
+  next_dst[0].addr_bytes[2] = 0x00;
+  next_dst[0].addr_bytes[3] = 0x33;
+  next_dst[0].addr_bytes[4] = 0x33;
+  next_dst[0].addr_bytes[5] = 0x33;
 
-  dst[1].addr_bytes[0] = 0xa0;
-  dst[1].addr_bytes[1] = 0x36;
-  dst[1].addr_bytes[2] = 0x9f;
-  dst[1].addr_bytes[3] = 0x39;
-  dst[1].addr_bytes[4] = 0x10;
-  dst[1].addr_bytes[5] = 0x4c;
+  next_dst[1].addr_bytes[0] = 0xa0;
+  next_dst[1].addr_bytes[1] = 0x36;
+  next_dst[1].addr_bytes[2] = 0x9f;
+  next_dst[1].addr_bytes[3] = 0x39;
+  next_dst[1].addr_bytes[4] = 0x10;
+  next_dst[1].addr_bytes[5] = 0x4c;
 
-  rte_eth_macaddr_get(0, &src[0]);
-  rte_eth_macaddr_get(1, &src[1]);
+  rte_eth_macaddr_get(0, &next_src[1]);
+  rte_eth_macaddr_get(1, &next_src[0]);
 
-  printf("flow[0->1]: %s -> %s \n", to_str(&src[0]).c_str(), to_str(&dst[0]).c_str());
-  printf("flow[1->0]: %s -> %s \n", to_str(&src[1]).c_str(), to_str(&dst[1]).c_str());
+  printf("flow[0->1]: %s -> %s \n", to_str(&next_src[0]).c_str(), to_str(&next_dst[0]).c_str());
+  printf("flow[1->0]: %s -> %s \n", to_str(&next_src[1]).c_str(), to_str(&next_dst[1]).c_str());
 
   rte_eal_remote_launch(l2fwd, nullptr, 1);
   rte_eal_mp_wait_lcore();
